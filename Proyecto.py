966e03d3856d7e65e4b01959890c53b9ebb00a12
Germanc97 <germanc97@javerianacali.edu.co>
2016-11-01 17:46:45 -0500

    Ya tiene el menu de los niveles, pero faltan los botones

--- Proyecto.py
+++ Proyecto.py
@@ -5,25 +5,28 @@ v = Tk()
 
 #menu
 
-
-
-
-
-def niveles():
-      """
-      """
-      canvasi = Canvas(v, width = 1200, height = 700)   
-      canvasi.focus_set()
-      canvasi.pack()
-
-
 canvasi = Canvas(v, width = 1200, height = 700,)   
 canvasi.focus_set()
 canvasi.pack()
 menu = PhotoImage(file = './menuinicio.gif')
 canvasi.create_image(630,350, image=menu)
 
-
-button1 = Button( text = "Quit", command = niveles, width= 30).place(x=500,y=410)
+def niveles():
+      """
+      """
+      global v, canvasi, menu, menuniveles
+      
+      v.destroy()
+      e = Tk()
+      canvasn = Canvas(e, width = 1200, height = 700)
+      canvasn.pack()
+      canvasn.focus_set()
+      menuniveles = PhotoImage(file = './menuniveles.gif')
+      canvasn.create_image(600,350, image = menuniveles)
+      e.mainloop()
+      
+      
+b1 = Button(master = None, text = "Elegir Nivel", width= 30, height=1, command = niveles).place(x=450,y=410)
 v.mainloop()
+
 
